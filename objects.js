b569830d927e4818d5b3ea03e3cd38fd1a383f79
Justin Chang <justinxchang@hotmail.com>
2018-08-17 20:39:29 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,15 +1,15 @@
-// var playlist = {
-//   Yiruma: "Kiss the Rain",
-//   ATB: "Ecstasy",
-//   LilWayne: "President Carter"
-// }
+var playlist = {
+  Yiruma: "Kiss the Rain",
+  ATB: "Ecstasy",
+  LilWayne: "President Carter"
+}
 
-// function updatePlaylist(playlist, artist, song){
-//   playlist[artist] = song
-//   return playlist
-// }
+function updatePlaylist(playlist, artist, song){
+  playlist[artist] = song
+  return playlist
+}
 
-// function removeFromPlaylist(playlist, artist){
-//   delete playlist.artist
-//   return playlist
-// }
+function removeFromPlaylist(playlist, artist){
+  delete playlist.artist
+  return playlist
+}
